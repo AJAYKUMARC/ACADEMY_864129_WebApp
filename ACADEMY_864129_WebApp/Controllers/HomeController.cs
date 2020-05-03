1f4946a0221c703eb2f6ee8f4c7d0481f37932bb
ajaykumarchepuri1 <ajaykumarchepuri1@gmail.com>
2020-05-04 00:42:17 +0530

    Add project files.

---
+++ ACADEMY_864129_WebApp/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ACADEMY_864129_WebApp.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace ACADEMY_864129_WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<DeviceData> deviceDataList = new List<DeviceData>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44383/api/Device/GetTelemetryDataTable"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    deviceDataList = JsonConvert.DeserializeObject<List<DeviceData>>(apiResponse);
+                }
+            }
+            return View(deviceDataList);
+        }
+
+        public IActionResult ViewDevices()
+        {
+            return View("DevicesConnected");
+        }
+
+        public IActionResult ViewConfigurations()
+        {
+            return View("ViewConfigurations");
+        }
+    }
+}
---
+++ ACADEMY_864129_WebApp/Models/DeviceData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ACADEMY_864129_WebApp.Models
+{
+    public class DeviceData : TableEntity
+    {
+        public string DeviceId { get; set; }
+        public string CaptureTime { get; set; }
+        public decimal DeviceLong { get; set; }
+        public decimal DeviceLat { get; set; }
+        public decimal Temperature { get; set; }
+        public string TemperatureUnit { get; set; }
+        public decimal Humidity { get; set; }
+        public string HumidityUnit { get; set; }
+        public bool DoorStatus { get; set; }
+
+    }
+}
